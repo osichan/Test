4781047f6422248c648f73044d06896b4dd69e03
Oskar Saienko <oskarsajenko@gmail.com>
2023-03-12 19:29:45 +0200

    comit git tets

---
+++ src/iot/model/Student.java
@@ -0,0 +1,17 @@
+package iot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Student {
+    private String name;
+    private String surname;
+}
